5d6e41195c756a20985fb5d0eefaee4b4664e937
andregaito <andre.gaito@gradu.iq.ufrj.br>
2025-07-31 19:58:21 +0000

    Deploying to gh-pages from @ andregaito/andregaito.github.io@c91bb5a664f1d31562e0df2c31e927ffc4c77e36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-gaito-labs",
+    title: "Gaito Labs",
     section: "Navigation",
     handler: () => {
       window.location.href = "/andregaito/";
